c86c028016c285eda092ce7ee944cd7bfe765745
Keshopan Chelvan <kchelvan@vyana.io>
2023-12-27 22:11:31 -0500

    Initialized Gallery for Landing Page

---
+++ app/utils/galleryImagePaths.ts
@@ -0,0 +1,20 @@
+import montreal_one from '@/public/images/gallery/montreal-2023/mt-1.jpg';
+import montreal_two from '@/public/images/gallery/montreal-2023/mt-2.jpg';
+import montreal_three from '@/public/images/gallery/montreal-2023/mt-3.jpg';
+import montreal_four from '@/public/images/gallery/montreal-2023/mt-4.jpg';
+import montreal_five from '@/public/images/gallery/montreal-2023/mt-5.jpg';
+import montreal_six from '@/public/images/gallery/montreal-2023/mt-6.jpg';
+
+export const GALLERY_IMAGES = {
+	montreal: [
+		montreal_one,
+		montreal_two,
+		montreal_four,
+		montreal_five,
+		montreal_six,
+		montreal_three,
+	],
+	selfPortrait: [],
+	street: [],
+	studio: [],
+};
