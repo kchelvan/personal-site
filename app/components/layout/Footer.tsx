a6f589dc592e091ebdeb1879c730ebea72a31db6
Keshopan Chelvan <kchelvan@vyana.io>
2024-01-01 18:00:44 -0500

    Improved Page Responsiveness for mobile

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -5,37 +5,39 @@ import Link from 'next/link';
 
 const Footer = () => {
 	return (
-		<div className={`bg-[#1C1C1D] h-full w-full p-16`}>
-			<div className='flex flex-row'>
-				<div>
-					<p className={`text-2xl text-[#FAFAFA] mb-1`}>SOCIALS</p>
+		<div className={`bg-[#1C1C1D] h-full w-full p-16 `}>
+			<div className='lg:max-w-[1600px] lg:mx-auto lg:px-16'>
+				<div className='flex flex-row'>
 					<div>
-						<Link href={'https://www.linkedin.com/in/keshopan-chelvan/'}>
-							<p className={`text-base sub-text text-[#FAFAFA] underline`}>
-								LinkedIn
-							</p>
-						</Link>
-						<Link href={'https://www.instagram.com/keshopan/'}>
-							<p className={`text-base sub-text text-[#FAFAFA] underline`}>
-								Instagram
-							</p>
-						</Link>
-						<Link href={'https://github.com/kchelvan'}>
-							<p className={`text-base sub-text text-[#FAFAFA] underline`}>
-								Github
-							</p>
-						</Link>
+						<p className={`text-2xl text-[#FAFAFA] mb-1`}>SOCIALS</p>
+						<div>
+							<Link href={'https://www.linkedin.com/in/keshopan-chelvan/'}>
+								<p className={`text-base sub-text text-[#FAFAFA] underline`}>
+									LinkedIn
+								</p>
+							</Link>
+							<Link href={'https://www.instagram.com/keshopan/'}>
+								<p className={`text-base sub-text text-[#FAFAFA] underline`}>
+									Instagram
+								</p>
+							</Link>
+							<Link href={'https://github.com/kchelvan'}>
+								<p className={`text-base sub-text text-[#FAFAFA] underline`}>
+									Github
+								</p>
+							</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className='flex flex-col md:flex-row justify-between'>
-				<p className={`text-base sub-text text-[#FAFAFA] mt-8`}>
-					© 2023 Keshopan Chelvan
-				</p>
-				<p className={`text-base sub-text text-[#FAFAFA] mt-8`}>
-					Designed and Developed by Keshopan using NextJS. <br />
-					Webpage is still in development.
-				</p>
+				<div className='flex flex-col md:flex-row justify-between'>
+					<p className={`text-base sub-text text-[#FAFAFA] mt-8`}>
+						© 2023 Keshopan Chelvan
+					</p>
+					<p className={`text-base sub-text text-[#FAFAFA] mt-8`}>
+						Designed and Developed by Keshopan using NextJS. <br />
+						Webpage is still in development.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
--- app/features/Contact/index.tsx
+++ app/features/Contact/index.tsx
@@ -6,7 +6,12 @@ const Contact = () => {
 			<p id='contact' className='pt-32 text-5xl my-8 text-[#928b7b]'>
 				Contact.
 			</p>
-			<p>Coming soon. Please refer to Socials in Footer.</p>
+			<p className='text-lg my-8 xl:w-2/5'>
+				I am currently available for freelance work. Please contact me using my
+				socials in the footer. <br />
+				<br />I look forward to working together soon!
+			</p>
+			<p></p>
 		</div>
 	);
 };
