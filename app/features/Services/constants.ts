6b69be1a34877fd1ed265af1a0b38db5bbb666c5
kchelvan <keshopan.chelvan@gmail.com>
2024-04-16 20:28:52 -0400

    Added GraphQL AWS and Firebase services

--- app/features/Services/constants.ts
+++ app/features/Services/constants.ts
@@ -6,6 +6,9 @@ import typescript from '@/public/logo/typescript.png';
 import vercel from '@/public/logo/vercel.png';
 import prisma from '@/public/logo/prisma.png';
 import salesforce from '@/public/logo/salesforce.png';
+import graphql from '@/public/logo/graphql.png';
+import aws from '@/public/logo/aws.png';
+import firebase from '@/public/logo/firebase.png';
 
 export const SERVICES = [
 	'Web Development',
@@ -24,6 +27,9 @@ export const TECH_STACK = [
 	{ label: 'Prisma', icon: prisma },
 	{ label: 'Vercel', icon: vercel },
 	{ label: 'Salesforce', icon: salesforce },
+	{ label: 'GraphQL', icon: graphql },
+	{ label: 'AWS', icon: aws },
+	{ label: 'Firebase', icon: firebase },
 ];
 
 export type TECH_STACK_TYPE = {
